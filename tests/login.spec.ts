5a1e5411cc338f7a0e44ccfe7fa3a245fc016556
T B <filo74b@gmail.com>
2025-06-26 00:58:35 +0200

    Registration update
    add temporary registration flow

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -7,3 +7,26 @@ test('test', async ({ page }) => {
   await page.locator('form').filter({ hasText: 'Signup' }).getByPlaceholder('Email Address').click();
   await page.locator('form').filter({ hasText: 'Signup' }).getByPlaceholder('Email Address').fill('testuser42291@gmail.com');
   await page.getByRole('button', { name: 'Signup' }).click();
+
+
+  
+  await page.getByRole('textbox', { name: 'First Name' }).click();
+  await page.getByRole('textbox', { name: 'First Name' }).fill('Test');
+  await page.getByRole('textbox', { name: 'Last Name' }).click();
+  await page.getByRole('textbox', { name: 'Last Name' }).fill('User');
+  await page.getByRole('textbox', { name: 'Address' }).click();
+  await page.getByRole('textbox', { name: 'Address' }).fill('Test Address');
+  await page.getByRole('textbox', { name: 'Company' }).click();
+  await page.getByRole('textbox', { name: 'Company' }).fill('Test Company');
+  await page.getByRole('textbox', { name: 'State' }).click();
+  await page.getByRole('textbox', { name: 'State' }).fill('Test State');
+  await page.getByRole('textbox', { name: 'City' }).click();
+  await page.getByRole('textbox', { name: 'City' }).fill('Test City');
+  await page.getByRole('textbox', { name: 'Zipcode' }).click();
+  await page.getByRole('textbox', { name: 'Zipcode' }).fill('12345');
+  await page.getByRole('textbox', { name: 'Mobile Number' }).click();
+  await page.getByRole('textbox', { name: 'Mobile Number' }).fill('1234567890');
+  await page.getByRole('button', { name: 'Create Account' }).click();
+  await page.getByRole('link', { name: 'Continue' }).click();
+  await page.getByRole('link', { name: 'View Cart' }).click();
+  
